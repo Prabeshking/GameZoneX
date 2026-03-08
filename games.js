91fd02840154710f1e9f9bce8ad2bd5c58174765
Prabeshking <bhandariprabesh013@gmail.com>
2026-03-08 21:35:38 +0100

    Update games.js

--- games.js
+++ games.js
@@ -1,349 +1,109 @@
 const games = [
 
-    /* ===== RACING ===== */
+    /* ============================
+       RACING (IFRAME SAFE)
+    ============================ */
     {
         id: "slope",
         title: "Slope",
         category: "Racing",
-        url: "https://ubg98.github.io/Slope/",
-        thumbnail: "https://ubg98.github.io/Slope/icon.png"
+        url: "https://gfiles.io/gfiles/games/slope/",
+        thumbnail: "thumbnails/slope.png"
     },
     {
         id: "drift-boss",
         title: "Drift Boss",
         category: "Racing",
-        url: "https://ubg77.github.io/game131022/drive-boss/",
-        thumbnail: "https://ubg77.github.io/game131022/drive-boss/icon.png"
+        url: "https://3kh0.github.io/drift-boss/",
+        thumbnail: "thumbnails/drift-boss.png"
     },
     {
         id: "moto-x3m",
         title: "Moto X3M",
         category: "Racing",
-        url: "https://ubg77.github.io/game131022/moto-x3m/",
-        thumbnail: "https://ubg77.github.io/game131022/moto-x3m/icon.png"
+        url: "https://3kh0.github.io/moto-x3m/",
+        thumbnail: "thumbnails/moto-x3m.png"
     },
     {
         id: "moto-x3m-pool",
         title: "Moto X3M Pool Party",
         category: "Racing",
-        url: "https://ubg77.github.io/game131022/moto-x3m-pool/",
-        thumbnail: "https://ubg77.github.io/game131022/moto-x3m-pool/icon.png"
+        url: "https://3kh0.github.io/moto-x3m-pool/",
+        thumbnail: "thumbnails/moto-x3m-pool.png"
     },
     {
         id: "moto-x3m-spooky",
         title: "Moto X3M Spooky",
         category: "Racing",
-        url: "https://ubg77.github.io/game131022/moto-x3m-spooky/",
-        thumbnail: "https://ubg77.github.io/game131022/moto-x3m-spooky/icon.png"
+        url: "https://3kh0.github.io/moto-x3m-spooky/",
+        thumbnail: "thumbnails/moto-x3m-spooky.png"
     },
     {
         id: "snow-rider-3d",
         title: "Snow Rider 3D",
         category: "Racing",
-        url: "https://ubg77.github.io/game131022/snow-rider-3d/",
-        thumbnail: "https://ubg77.github.io/game131022/snow-rider-3d/icon.png"
+        url: "https://ubg-unblocked.github.io/snow-rider-3d/",
+        thumbnail: "thumbnails/snow-rider-3d.png"
     },
-
-    /* ===== ENDLESS RUNNER ===== */
-    {
-        id: "subway-surfers",
-        title: "Subway Surfers",
-        category: "Endless Runner",
-        url: "https://ubg77.github.io/game131022/subway-surfers/",
-        thumbnail: "https://ubg77.github.io/game131022/subway-surfers/icon.png"
-    },
-    {
-        id: "temple-run-2",
-        title: "Temple Run 2",
-        category: "Endless Runner",
-        url: "https://ubg77.github.io/game131022/temple-run-2/",
-        thumbnail: "https://ubg77.github.io/game131022/temple-run-2/icon.png"
-    },
-    {
-        id: "run-3",
-        title: "Run 3",
-        category: "Endless Runner",
-        url: "https://ubg77.github.io/game131022/run-3/",
-        thumbnail: "https://ubg77.github.io/game131022/run-3/icon.png"
-    },
-    {
-        id: "run-2",
-        title: "Run 2",
-        category: "Endless Runner",
-        url: "https://ubg77.github.io/game131022/run-2/",
-        thumbnail: "https://ubg77.github.io/game131022/run-2/icon.png"
-    },
-    {
-        id: "run-1",
-        title: "Run 1",
-        category: "Endless Runner",
-        url: "https://ubg77.github.io/game131022/run-1/",
-        thumbnail: "https://ubg77.github.io/game131022/run-1/icon.png"
-    },
-
-    /* ===== EXTRA RACING ===== */
     {
         id: "escape-road",
         title: "Escape Road",
         category: "Racing",
-        url: "https://ubg77.github.io/game131022/escape-road/",
-        thumbnail: "https://ubg77.github.io/game131022/escape-road/icon.png"
+        url: "https://ubg-unblocked.github.io/escape-road/",
+        thumbnail: "thumbnails/escape-road.png"
     },
     {
         id: "slope-rider",
         title: "Slope Rider",
         category: "Racing",
-        url: "https://ubg77.github.io/game131022/slope-rider/",
-        thumbnail: "https://ubg77.github.io/game131022/slope-rider/icon.png"
+        url: "https://ubg-unblocked.github.io/slope-rider/",
+        thumbnail: "thumbnails/slope-rider.png"
     },
     {
         id: "snow-rider",
         title: "Snow Rider",
         category: "Racing",
-        url: "https://ubg77.github.io/game131022/snow-rider/",
-        thumbnail: "https://ubg77.github.io/game131022/snow-rider/icon.png"
+        url: "https://ubg-unblocked.github.io/snow-rider/",
+        thumbnail: "thumbnails/snow-rider.png"
     },
 
-    /* ===== PLATFORMER ===== */
+    /* ============================
+       ENDLESS RUNNER (IFRAME SAFE)
+    ============================ */
     {
-        id: "vex-3",
-        title: "Vex 3",
-        category: "Platformer",
-        url: "https://ubg77.github.io/game131022/vex3/",
-        thumbnail: "https://ubg77.github.io/game131022/vex3/icon.png"
+        id: "subway-surfers",
+        title: "Subway Surfers",
+        category: "Endless Runner",
+        url: "https://3kh0.github.io/subway-surfers/",
+        thumbnail: "thumbnails/subway-surfers.png"
     },
     {
-        id: "vex-4",
-        title: "Vex 4",
-        category: "Platformer",
-        url: "https://ubg77.github.io/game131022/vex4/",
-        thumbnail: "https://ubg77.github.io/game131022/vex4/icon.png"
+        id: "temple-run-2",
+        title: "Temple Run 2",
+        category: "Endless Runner",
+        url: "https://3kh0.github.io/temple-run-2/",
+        thumbnail: "thumbnails/temple-run-2.png"
     },
     {
-        id: "vex-5",
-        title: "Vex 5",
-        category: "Platformer",
-        url: "https://ubg77.github.io/game131022/vex5/",
-        thumbnail: "https://ubg77.github.io/game131022/vex5/icon.png"
+        id: "run-3",
+        title: "Run 3",
+        category: "Endless Runner",
+        url: "https://gfiles.io/gfiles/games/run-3/",
+        thumbnail: "thumbnails/run-3.png"
     },
     {
-        id: "vex-6",
-        title: "Vex 6",
-        category: "Platformer",
-        url: "https://ubg77.github.io/game131022/vex6/",
-        thumbnail: "https://ubg77.github.io/game131022/vex6/icon.png"
+        id: "run-2",
+        title: "Run 2",
+        category: "Endless Runner",
+        url: "https://gfiles.io/gfiles/games/run-2/",
+        thumbnail: "thumbnails/run-2.png"
     },
     {
-        id: "vex-7",
-        title: "Vex 7",
-        category: "Platformer",
-        url: "https://ubg77.github.io/game131022/vex7/",
-        thumbnail: "https://ubg77.github.io/game131022/vex7/icon.png"
-    },
-
-    /* ===== PUZZLE ===== */
-    {
-        id: "cut-the-rope",
-        title: "Cut the Rope",
-        category: "Puzzle",
-        url: "https://ubg77.github.io/game131022/cut-the-rope/",
-        thumbnail: "https://ubg77.github.io/game131022/cut-the-rope/icon.png"
-    },
-    {
-        id: "2048",
-        title: "2048",
-        category: "Puzzle",
-        url: "https://ubg77.github.io/game131022/2048/",
-        thumbnail: "https://ubg77.github.io/game131022/2048/icon.png"
-    },
-    {
-        id: "bad-ice-cream",
-        title: "Bad Ice Cream",
-        category: "Puzzle",
-        url: "https://ubg77.github.io/game131022/bad-ice-cream/",
-        thumbnail: "https://ubg77.github.io/game131022/bad-ice-cream/icon.png"
-    },
-    {
-        id: "bad-ice-cream-2",
-        title: "Bad Ice Cream 2",
-        category: "Puzzle",
-        url: "https://ubg77.github.io/game131022/bad-ice-cream-2/",
-        thumbnail: "https://ubg77.github.io/game131022/bad-ice-cream-2/icon.png"
-    },
-    {
-        id: "bad-ice-cream-3",
-        title: "Bad Ice Cream 3",
-        category: "Puzzle",
-        url: "https://ubg77.github.io/game131022/bad-ice-cream-3/",
-        thumbnail: "https://ubg77.github.io/game131022/bad-ice-cream-3/icon.png"
+        id: "run-1",
+        title: "Run 1",
+        category: "Endless Runner",
+        url: "https://gfiles.io/gfiles/games/run/",
+        thumbnail: "thumbnails/run-1.png"
     },
 
-    /* ===== ARCADE ===== */
-    {
-        id: "stickman-hook",
-        title: "Stickman Hook",
-        category: "Arcade",
-        url: "https://ubg77.github.io/game131022/stickman-hook/",
-        thumbnail: "https://ubg77.github.io/game131022/stickman-hook/icon.png"
-    },
-    {
-        id: "geometry-dash-lite",
-        title: "Geometry Dash Lite",
-        category: "Arcade",
-        url: "https://ubg77.github.io/game131022/geometry-dash-lite/",
-        thumbnail: "https://ubg77.github.io/game131022/geometry-dash-lite/icon.png"
-    },
-    {
-        id: "tunnel-rush",
-        title: "Tunnel Rush",
-        category: "Arcade",
-        url: "https://ubg77.github.io/game131022/tunnel-rush/",
-        thumbnail: "https://ubg77.github.io/game131022/tunnel-rush/icon.png"
-    },
-    {
-        id: "tunnel-rush-2",
-        title: "Tunnel Rush 2",
-        category: "Arcade",
-        url: "https://ubg77.github.io/game131022/tunnel-rush-2/",
-        thumbnail: "https://ubg77.github.io/game131022/tunnel-rush-2/icon.png"
-    },
-    {
-        id: "space-waver",
-        title: "Space Waver",
-        category: "Arcade",
-        url: "https://ubg77.github.io/game131022/space-waver/",
-        thumbnail: "https://ubg77.github.io/game131022/space-waver/icon.png"
-    },
 
-    /* ===== RAGDOLL ===== */
-    {
-        id: "getting-over-it",
-        title: "Getting Over It",
-        category: "Ragdoll",
-        url: "https://ubg77.github.io/game131022/getting-over-it/",
-        thumbnail: "https://ubg77.github.io/game131022/getting-over-it/icon.png"
-    },
-    {
-        id: "happy-wheels",
-        title: "Happy Wheels",
-        category: "Ragdoll",
-        url: "https://ubg77.github.io/game131022/happy-wheels/",
-        thumbnail: "https://ubg77.github.io/game131022/happy-wheels/icon.png"
-    },
-
-    /* ===== SHOOTER ===== */
-    {
-        id: "1v1-lol",
-        title: "1v1.lol",
-        category: "Shooter",
-        url: "https://ubg77.github.io/game131022/1v1-lol/",
-        thumbnail: "https://ubg77.github.io/game131022/1v1-lol/icon.png"
-    },
-    {
-        id: "shell-shockers",
-        title: "Shell Shockers",
-        category: "Shooter",
-        url: "https://ubg77.github.io/game131022/shell-shockers/",
-        thumbnail: "https://ubg77.github.io/game131022/shell-shockers/icon.png"
-    },
-    {
-        id: "krunker",
-        title: "Krunker",
-        category: "Shooter",
-        url: "https://ubg77.github.io/game131022/krunker-io/",
-        thumbnail: "https://ubg77.github.io/game131022/krunker-io/icon.png"
-    },
-
-    /* ===== BATTLE ROYALE ===== */
-    {
-        id: "zombs-royale",
-        title: "Zombs Royale",
-        category: "Battle Royale",
-        url: "https://ubg77.github.io/game131022/zombs-royale/",
-        thumbnail: "https://ubg77.github.io/game131022/zombs-royale/icon.png"
-    },
-
-    /* ===== IO ===== */
-    {
-        id: "paper-io-2",
-        title: "Paper.io 2",
-        category: "IO",
-        url: "https://ubg77.github.io/game131022/paper-io-2/",
-        thumbnail: "https://ubg77.github.io/game131022/paper-io-2/icon.png"
-    },
-
-    /* ===== SPORTS ===== */
-    {
-        id: "basketball-stars",
-        title: "Basketball Stars",
-        category: "Sports",
-        url: "https://ubg77.github.io/game131022/basketball-stars/",
-        thumbnail: "https://ubg77.github.io/game131022/basketball-stars/icon.png"
-    },
-    {
-        id: "soccer-skills",
-        title: "Soccer Skills Euro Cup",
-        category: "Sports",
-        url: "https://ubg77.github.io/game131022/soccer-skills-euro-cup/",
-        thumbnail: "https://ubg77.github.io/game131022/soccer-skills-euro-cup/icon.png"
-    },
-    {
-        id: "retro-bowl",
-        title: "Retro Bowl",
-        category: "Sports",
-        url: "https://ubg77.github.io/game131022/retro-bowl/",
-        thumbnail: "https://ubg77.github.io/game131022/retro-bowl/icon.png"
-    },
-
-    /* ===== RETRO ===== */
-    {
-        id: "super-mario-bros",
-        title: "Super Mario Bros",
-        category: "Retro",
-        url: "https://ubg77.github.io/game131022/super-mario-bros/",
-        thumbnail: "https://ubg77.github.io/game131022/super-mario-bros/icon.png"
-    },
-    {
-        id: "super-mario-63",
-        title: "Super Mario 63",
-        category: "Retro",
-        url: "https://ubg77.github.io/game131022/super-mario-63/",
-        thumbnail: "https://ubg77.github.io/game131022/super-mario-63/icon.png"
-    },
-    {
-        id: "super-mario-flash",
-        title: "Super Mario Flash",
-        category: "Retro",
-        url: "https://ubg77.github.io/game131022/super-mario-flash/",
-        thumbnail: "https://ubg77.github.io/game131022/super-mario-flash/icon.png"
-    },
-    {
-        id: "super-mario-world",
-        title: "Super Mario World",
-        category: "Retro",
-        url: "https://ubg77.github.io/game131022/super-mario-world/",
-        thumbnail: "https://ubg77.github.io/game131022/super-mario-world/icon.png"
-    },
-    {
-        id: "sonic-classic",
-        title: "Sonic Classic",
-        category: "Retro",
-        url: "https://ubg77.github.io/game131022/sonic-classic/",
-        thumbnail: "https://ubg77.github.io/game131022/sonic-classic/icon.png"
-    },
-    {
-        id: "sonic-advance",
-        title: "Sonic Advance",
-        category: "Retro",
-        url: "https://ubg77.github.io/game131022/sonic-advance/",
-        thumbnail: "https://ubg77.github.io/game131022/sonic-advance/icon.png"
-    },
-    {
-        id: "sonic-2",
-        title: "Sonic 2",
-        category: "Retro",
-        url: "https://ubg77.github.io/game131022/sonic-2/",
-        thumbnail: "https://ubg77.github.io/game131022/sonic-2/icon.png"
-    }
-
-]; // END
