67a1c9c47435056e4171e80e5ff80a5d286a9fe2
Prabeshking <bhandariprabesh013@gmail.com>
2026-03-09 07:21:07 +0100

    Update games.js

--- games.js
+++ games.js
@@ -105,5 +105,341 @@ const games = [
         url: "https://gfiles.io/gfiles/games/run/",
         thumbnail: "thumbnails/run-1.png"
     },
+        /* ============================
+       PLATFORMER (IFRAME SAFE)
+    ============================ */
+    {
+        id: "vex-3",
+        title: "Vex 3",
+        category: "Platformer",
+        url: "https://3kh0.github.io/vex3/",
+        thumbnail: "thumbnails/vex-3.png"
+    },
+    {
+        id: "vex-4",
+        title: "Vex 4",
+        category: "Platformer",
+        url: "https://3kh0.github.io/vex4/",
+        thumbnail: "thumbnails/vex-4.png"
+    },
+    {
+        id: "vex-5",
+        title: "Vex 5",
+        category: "Platformer",
+        url: "https://3kh0.github.io/vex5/",
+        thumbnail: "thumbnails/vex-5.png"
+    },
+    {
+        id: "vex-6",
+        title: "Vex 6",
+        category: "Platformer",
+        url: "https://3kh0.github.io/vex6/",
+        thumbnail: "thumbnails/vex-6.png"
+    },
+    {
+        id: "vex-7",
+        title: "Vex 7",
+        category: "Platformer",
+        url: "https://3kh0.github.io/vex7/",
+        thumbnail: "thumbnails/vex-7.png"
+    },
+
+    /* ============================
+       PUZZLE (IFRAME SAFE)
+    ============================ */
+    {
+        id: "cut-the-rope",
+        title: "Cut the Rope",
+        category: "Puzzle",
+        url: "https://3kh0.github.io/cut-the-rope/",
+        thumbnail: "thumbnails/cut-the-rope.png"
+    },
+    {
+        id: "2048",
+        title: "2048",
+        category: "Puzzle",
+        url: "https://gfiles.io/gfiles/games/2048/",
+        thumbnail: "thumbnails/2048.png"
+    },
+    {
+        id: "bad-ice-cream",
+        title: "Bad Ice Cream",
+        category: "Puzzle",
+        url: "https://ubg-unblocked.github.io/bad-ice-cream/",
+        thumbnail: "thumbnails/bad-ice-cream.png"
+    },
+    {
+        id: "bad-ice-cream-2",
+        title: "Bad Ice Cream 2",
+        category: "Puzzle",
+        url: "https://ubg-unblocked.github.io/bad-ice-cream-2/",
+        thumbnail: "thumbnails/bad-ice-cream-2.png"
+    },
+    {
+        id: "bad-ice-cream-3",
+        title: "Bad Ice Cream 3",
+        category: "Puzzle",
+        url: "https://ubg-unblocked.github.io/bad-ice-cream-3/",
+        thumbnail: "thumbnails/bad-ice-cream-3.png"
+    },
+
+    /* ============================
+       ARCADE (IFRAME SAFE)
+    ============================ */
+    {
+        id: "stickman-hook",
+        title: "Stickman Hook",
+        category: "Arcade",
+        url: "https://3kh0.github.io/stickman-hook/",
+        thumbnail: "thumbnails/stickman-hook.png"
+    },
+    {
+        id: "geometry-dash-lite",
+        title: "Geometry Dash Lite",
+        category: "Arcade",
+        url: "https://3kh0.github.io/geometry-dash-lite/",
+        thumbnail: "thumbnails/geometry-dash-lite.png"
+    },
+    {
+        id: "tunnel-rush",
+        title: "Tunnel Rush",
+        category: "Arcade",
+        url: "https://3kh0.github.io/tunnel-rush/",
+        thumbnail: "thumbnails/tunnel-rush.png"
+    },
+    {
+        id: "tunnel-rush-2",
+        title: "Tunnel Rush 2",
+        category: "Arcade",
+        url: "https://3kh0.github.io/tunnel-rush-2/",
+        thumbnail: "thumbnails/tunnel-rush-2.png"
+    },
+    {
+        id: "space-waver",
+        title: "Space Waver",
+        category: "Arcade",
+        url: "https://ubg-unblocked.github.io/space-waver/",
+        thumbnail: "thumbnails/space-waver.png"
+    },
+
+    /* ============================
+       RAGDOLL (IFRAME SAFE)
+    ============================ */
+    {
+        id: "getting-over-it",
+        title: "Getting Over It",
+        category: "Ragdoll",
+        url: "https://3kh0.github.io/getting-over-it/",
+        thumbnail: "thumbnails/getting-over-it.png"
+    },
+    {
+        id: "happy-wheels",
+        title: "Happy Wheels",
+        category: "Ragdoll",
+        url: "https://3kh0.github.io/happy-wheels/",
+        thumbnail: "thumbnails/happy-wheels.png"
+    },
+    /* ============================
+       SHOOTER (IFRAME SAFE REPLACEMENTS)
+    ============================ */
+    {
+        id: "1v1-space",
+        title: "1v1.space",
+        category: "Shooter",
+        url: "https://3kh0.github.io/1v1-space/",
+        thumbnail: "thumbnails/1v1-space.png"
+    },
+    {
+        id: "venge-io",
+        title: "Venge.io",
+        category: "Shooter",
+        url: "https://3kh0.github.io/venge-io/",
+        thumbnail: "thumbnails/venge-io.png"
+    },
+    {
+        id: "project-laser",
+        title: "Project Laser",
+        category: "Shooter",
+        url: "https://ubg-unblocked.github.io/project-laser/",
+        thumbnail: "thumbnails/project-laser.png"
+    },
+
+    /* ============================
+       BATTLE ROYALE (IFRAME SAFE)
+    ============================ */
+    {
+        id: "surviv-io",
+        title: "Surviv.io",
+        category: "Battle Royale",
+        url: "https://3kh0.github.io/surviv-io/",
+        thumbnail: "thumbnails/surviv-io.png"
+    },
+    {
+        id: "mini-royale",
+        title: "Mini Royale",
+        category: "Battle Royale",
+        url: "https://ubg-unblocked.github.io/mini-royale/",
+        thumbnail: "thumbnails/mini-royale.png"
+    },
+
+    /* ============================
+       IO GAMES (IFRAME SAFE)
+    ============================ */
+    {
+        id: "paper-io-2",
+        title: "Paper.io 2",
+        category: "IO",
+        url: "https://3kh0.github.io/paper-io-2/",
+        thumbnail: "thumbnails/paper-io-2.png"
+    },
+    {
+        id: "slither-io",
+        title: "Slither.io",
+        category: "IO",
+        url: "https://ubg-unblocked.github.io/slither-io/",
+        thumbnail: "thumbnails/slither-io.png"
+    },
+    {
+        id: "agar-io",
+        title: "Agar.io",
+        category: "IO",
+        url: "https://ubg-unblocked.github.io/agar-io/",
+        thumbnail: "thumbnails/agar-io.png"
+    },
+
+    /* ============================
+       SPORTS (IFRAME SAFE)
+    ============================ */
+    {
+        id: "basketball-stars",
+        title: "Basketball Stars",
+        category: "Sports",
+        url: "https://3kh0.github.io/basketball-stars/",
+        thumbnail: "thumbnails/basketball-stars.png"
+    },
+    {
+        id: "soccer-skills",
+        title: "Soccer Skills Euro Cup",
+        category: "Sports",
+        url: "https://3kh0.github.io/soccer-skills/",
+        thumbnail: "thumbnails/soccer-skills.png"
+    },
+    {
+        id: "retro-bowl",
+        title: "Retro Bowl",
+        category: "Sports",
+        url: "https://3kh0.github.io/retro-bowl/",
+        thumbnail: "thumbnails/retro-bowl.png"
+    },
+
+    /* ============================
+       RETRO (HTML5 SAFE VERSIONS)
+    ============================ */
+    {
+        id: "super-mario-html5",
+        title: "Super Mario HTML5",
+        category: "Retro",
+        url: "https://3kh0.github.io/mario/",
+        thumbnail: "thumbnails/super-mario-html5.png"
+    },
+    {
+        id: "super-mario-63",
+        title: "Super Mario 63",
+        category: "Retro",
+        url: "https://3kh0.github.io/super-mario-63/",
+        thumbnail: "thumbnails/super-mario-63.png"
+    },
+    {
+        id: "sonic-reborn",
+        title: "Sonic Reborn",
+        category: "Retro",
+        url: "https://ubg-unblocked.github.io/sonic-reborn/",
+        thumbnail: "thumbnails/sonic-reborn.png"
+    },
+    /* ============================
+       ACTION (IFRAME SAFE)
+    ============================ */
+    {
+        id: "fireboy-watergirl-forest",
+        title: "Fireboy & Watergirl: Forest Temple",
+        category: "Action",
+        url: "https://3kh0.github.io/fireboy-and-watergirl-forest-temple/",
+        thumbnail: "thumbnails/fireboy-watergirl-forest.png"
+    },
+    {
+        id: "fireboy-watergirl-light",
+        title: "Fireboy & Watergirl: Light Temple",
+        category: "Action",
+        url: "https://3kh0.github.io/fireboy-and-watergirl-light-temple/",
+        thumbnail: "thumbnails/fireboy-watergirl-light.png"
+    },
+    {
+        id: "fireboy-watergirl-crystal",
+        title: "Fireboy & Watergirl: Crystal Temple",
+        category: "Action",
+        url: "https://3kh0.github.io/fireboy-and-watergirl-crystal-temple/",
+        thumbnail: "thumbnails/fireboy-watergirl-crystal.png"
+    },
+    {
+        id: "red-ball-4",
+        title: "Red Ball 4",
+        category: "Action",
+        url: "https://3kh0.github.io/red-ball-4/",
+        thumbnail: "thumbnails/red-ball-4.png"
+    },
+    {
+        id: "bob-the-robber",
+        title: "Bob The Robber",
+        category: "Action",
+        url: "https://3kh0.github.io/bob-the-robber/",
+        thumbnail: "thumbnails/bob-the-robber.png"
+    },
+    {
+        id: "bob-the-robber-2",
+        title: "Bob The Robber 2",
+        category: "Action",
+        url: "https://3kh0.github.io/bob-the-robber-2/",
+        thumbnail: "thumbnails/bob-the-robber-2.png"
+    },
+    {
+        id: "stealing-the-diamond",
+        title: "Stealing the Diamond",
+        category: "Action",
+        url: "https://3kh0.github.io/stealing-the-diamond/",
+        thumbnail: "thumbnails/stealing-the-diamond.png"
+    },
+    {
+        id: "escaping-the-prison",
+        title: "Escaping the Prison",
+        category: "Action",
+        url: "https://3kh0.github.io/escaping-the-prison/",
+        thumbnail: "thumbnails/escaping-the-prison.png"
+    },
+    {
+        id: "breaking-the-bank",
+        title: "Breaking the Bank",
+        category: "Action",
+        url: "https://3kh0.github.io/breaking-the-bank/",
+        thumbnail: "thumbnails/breaking-the-bank.png"
+    },
+    {
+        id: "infiltrating-the-airship",
+        title: "Infiltrating the Airship",
+        category: "Action",
+        url: "https://3kh0.github.io/infiltrating-the-airship/",
+        thumbnail: "thumbnails/infiltrating-the-airship.png"
+    },
+    // ...last game object here...
+    {
+        id: "infiltrating-the-airship",
+        title: "Infiltrating the Airship",
+        category: "Action",
+        url: "https://3kh0.github.io/infiltrating-the-airship/",
+        thumbnail: "thumbnails/infiltrating-the-airship.png"
+    }
+
+]; // END
+
+
 
 
